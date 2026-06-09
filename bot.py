4c730534c0b5f78d1405a874f88e9bfe265f5296
sachintailor865-dotcom <sachintailor865@gmail.com>
2026-06-09 16:47:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ from diffusers import StableDiffusionImg2ImgPipeline
 import torch
 from PIL import Image
 import io
-TOKEN = "YAHAN_APNA_TELEGRAM_TOKEN_PASTE_KARO"
+TOKEN = "8913238303:AAGzyRrVQCQopIQajWKPVIRd0jCgdEYyeQM"
 pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
-    "8913238303:AAGzyRrVQCQopIQajWKPVIRd0jCgdEYyeQM",
+    "Lykon/dreamshaper-8",
     torch_dtype=torch.float16
 )
 pipe = pipe.to("cpu")  # Railway free mein GPU nahi hota
